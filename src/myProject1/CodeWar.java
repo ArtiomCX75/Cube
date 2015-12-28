7f0ccf8d09543771e42641f28bab6b61b2ac667e
ArtiomCX75 <faa1192@gmail.com>
2015-12-28 20:00:28 +0300

    noth

---
+++ src/myProject1/CodeWar.java
@@ -0,0 +1,36 @@
+package myProject1;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class CodeWar {
+	
+public static String getColor(){
+	try{}
+	catch(Exception e){
+		
+	}
+	finally {
+		
+	}
+	int i = (int) (Math.random()*4);
+	if (i==0)
+		return "white";
+	if (i==1)
+		return "yellow";
+	if (i==2)
+		return "purple";
+	if (i==3)
+		return "red";
+	//"eee".index
+	
+	return null;
+	//System.out.println();
+	
+}
+
+	
+}
