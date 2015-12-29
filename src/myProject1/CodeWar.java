dac7e6324ea47a1064619b9403c487f11b9491c0
ArtiomCX75 <faa1192@gmail.com>
2015-12-29 17:58:43 +0300

    noth

--- src/myProject1/CodeWar.java
+++ src/myProject1/CodeWar.java
@@ -1,36 +1,14 @@
 package myProject1;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-
-import org.junit.Test;
-
 public class CodeWar {
-	
-public static String getColor(){
-	try{}
-	catch(Exception e){
-		
+	public static int sortDesc(final int num) {
+		String s = Integer.valueOf(num).toString();
+		String s2 = "";
+		for(int i=0;i<s.length();i++)
+		{
+			s2=s2+s.substring(s.length()-1-i, s.length()-i);
+		}
+		return Integer.valueOf(s2);
 	}
-	finally {
-		
-	}
-	int i = (int) (Math.random()*4);
-	if (i==0)
-		return "white";
-	if (i==1)
-		return "yellow";
-	if (i==2)
-		return "purple";
-	if (i==3)
-		return "red";
-	//"eee".index
-	
-	return null;
-	//System.out.println();
-	
-}
 
-	
 }
